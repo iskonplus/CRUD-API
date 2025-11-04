ce05a586d62727ca476e6073dc4726c6191b08d9
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-04 17:12:02 +0100

    feat: implemented endpoint get all users

---
+++ src/db.js
@@ -0,0 +1,14 @@
+export const users = [
+    {
+        id: 'a81bc81b-dead-4e5d-abff-90865d1e13b1',
+        username: 'Artur',
+        age: 38,
+        hobbies: ['sleep', 'chili']
+    },
+    {
+        id: 'a81bc81b-dead-4e5d-abff-90865d1e13b2',
+        username: 'Artur2',
+        age: 39,
+        hobbies: ['the same']
+    }
+];
--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 import http from 'node:http';
+import { users } from './db.js';
 
 const PORT = Number(process.env.PORT || 4000);
 
 const server = http.createServer(async (req, res) => {
+
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathSegments = url.pathname.split('/').filter(Boolean);
+
+    try {
+
+        if (req.method === 'GET' && url.pathname === '/api/users') {
+            res.writeHead(200, { "Content-Type": "application/json" });
+            return res.end(JSON.stringify(users));
+        }
+
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
 
 })
 
