a8e7907cacfd1e04b0849af03932f8f83c550992
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-05 21:12:08 +0100

    test: add successful POST /api/users test (returns 201 and created user with id)

--- __tests__/api.users.spec.ts
+++ __tests__/api.users.spec.ts
@@ -2,8 +2,15 @@ import request from 'supertest';
 import { server } from '../src/server';
 
 afterAll((done) => {
-  server.close(done);
+    server.close(done);
 });
+
+let userId;
+const incomingUser = {
+    username: 'Artur',
+    age: 38,
+    hobbies: ['reading']
+}
 
 describe('Users API', () => {
 
@@ -11,6 +18,14 @@ describe('Users API', () => {
         const res = await request(server).get('/api/users');
         expect(res.status).toBe(200);
         expect(res.body).toEqual([]);
+    });
+
+    test('POST create user valid -> 201, user', async () => {
+        const res = await request(server).post('/api/users').send(incomingUser);
+        expect(res.status).toBe(201);
+        expect(res.body.id).toBeDefined();
+        userId = res.body.id;
+        expect(res.body).toEqual({...incomingUser, id: userId});
     });
 
 });
