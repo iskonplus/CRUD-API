e0cbdaf8be64c9169991adc083bbf16103875513
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-05 19:16:06 +0100

    refactor: remove default users to start with empty in-memory database

--- src/db.ts
+++ src/db.ts
@@ -2,20 +2,7 @@ import { IncomingUser } from './types/incomingUser';
 import { User } from './types/user';
 import { getRandomId } from './utils';
 
-const users: User[] = [
-    {
-        id: 'a81bc81b-dead-4e5d-abff-90865d1e13b1',
-        username: 'Artur',
-        age: 38,
-        hobbies: ['sleep', 'chili']
-    },
-    {
-        id: 'a81bc81b-dead-4e5d-abff-90865d1e13b2',
-        username: 'Artur2',
-        age: 39,
-        hobbies: ['the same']
-    }
-];
+const users: User[] = [];
 
 export const userService = {
 
