85281c355ebd7fdc30eab1d00eb6a72f4bac9892
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-05 21:39:32 +0100

    test: add valid  GET /api/users/:id test (returns 200 and correct user data)

--- __tests__/api.users.spec.ts
+++ __tests__/api.users.spec.ts
@@ -1,17 +1,19 @@
 import request from 'supertest';
 import { server } from '../src/server';
 import { errorMsg } from '../src/utils';
+import { User } from '../src/types/user';
 
 afterAll((done) => {
     server.close(done);
 });
 
-let userId;
+let userId: string;
 const incomingUser = {
     username: 'Artur',
     age: 38,
     hobbies: ['reading']
 }
+let user: User;
 
 describe('Users API', () => {
 
@@ -26,7 +28,8 @@ describe('Users API', () => {
         expect(res.status).toBe(201);
         expect(res.body.id).toBeDefined();
         userId = res.body.id;
-        expect(res.body).toEqual({ ...incomingUser, id: userId });
+        user = { id: userId, ...incomingUser };
+        expect(res.body).toEqual(user);
     });
 
     test('POST create user invalid -> 400, error message', async () => {
@@ -35,6 +38,13 @@ describe('Users API', () => {
         expect(res.body).toEqual({
             message: errorMsg.invalid.required,
         });
+    });
+
+    test('Get user by id -> 200, user', async () => {
+        const res = await request(server).get(`/api/users/${userId}`);
+        expect(res.status).toBe(200);
+        expect(res.body.id).toBe(userId);
+        expect(res.body).toEqual(user);
     });
 
 });
